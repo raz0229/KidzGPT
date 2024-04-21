bcc080ea0ba79fde56052ed2354b5a284ab9b4c8
Muhammad Mubashir <114167473+mubbashirr@users.noreply.github.com>
2024-04-21 13:05:08 +0500

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,65 @@
 import streamlit as st
 import os
-from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text
+from openai import OpenAI
+from dotenv import load_dotenv
+import base64
 from audio_recorder_streamlit import audio_recorder
-from streamlit_float import *
+from streamlit_float import float_init
 
-# Float feature initialization
+# Initialize Streamlit Float feature
 float_init()
+
+client = OpenAI(api_key="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")
+
+# Utility functions
+
+def get_answer(messages):
+    system_message = [{"role": "system", "content": "You are an helpful AI chatbot for children, that answers questions asked by small kids between age of 3 to 7 years old. Assist the children's quries in such a way that you can teach something good in entertaining manner. Keep your output response as small as possible so that a 3 to 7 years old can easily understand it."}]
+    messages = system_message + messages
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return response.choices[0].message.content
+
+def speech_to_text(audio_data):
+    with open(audio_data, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            response_format="text",
+            file=audio_file
+        )
+    return transcript
+
+def text_to_speech(input_text):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="nova",
+        input=input_text
+    )
+    webm_file_path = "temp_audio_play.mp3"
+    with open(webm_file_path, "wb") as f:
+        response.stream_to_file(webm_file_path)
+    return webm_file_path
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+    b64 = base64.b64encode(data).decode("utf-8")
+    md = f"""
+    <audio autoplay>
+    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+    </audio>
+    """
+    st.markdown(md, unsafe_allow_html=True)
+
+# Main application code
 
 def initialize_session_state():
     if "messages" not in st.session_state:
         st.session_state.messages = [
             {"role": "assistant", "content": "Hi! How may I assist you today?"}
         ]
-    # if "audio_initialized" not in st.session_state:
-    #     st.session_state.audio_initialized = False
 
 initialize_session_state()
 
@@ -23,7 +69,6 @@ st.title("OpenAI Conversational Chatbot 🤖")
 footer_container = st.container()
 with footer_container:
     audio_bytes = audio_recorder()
-
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
